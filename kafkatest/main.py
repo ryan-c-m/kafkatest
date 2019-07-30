dcb399ad5bf5a1fbc661f2a31f0788b2e7623cf8
Ryan Morris <ryan.c.morris1@gmail.com>
2019-07-30 20:43:10 +1000

    Separate send and consume

--- kafkatest/main.py
+++ kafkatest/main.py
@@ -3,26 +3,25 @@ Main module
 """
 import time
 from kafka import KafkaConsumer, KafkaProducer
-from kafka.errors import KafkaError
+
 
 class KafkaTest:
     """
     KafkaTest class
     """
 
-    def __init__(self):
+    def __init__(self, timeout=10):
         self.producer_topic = None
         self.producer = None
         self.consumer = None
-        self.messages = []
-        self.timeout=10
+        self.messages = {}
+        self.timeout = timeout
 
     def configure_producer(self, topic, kafka_producer):
         """
         Configure with a KafkaProducer
-        :param topic:
-        :param kafka_producer:
-        :return:
+        :param topic: topic to send messages to
+        :param kafka_producer: a KafkaProducer
         """
         self.producer_topic = topic
         self.producer = kafka_producer
@@ -30,26 +29,35 @@ class KafkaTest:
     def configure_consumer(self, kafka_consumer):
         """
         Configure with a KafkaConsumer
-        :param kafka_consumer:
-        :return:
+        :param kafka_consumer: a KafkaConsumer
         """
         self.consumer = kafka_consumer
 
-    def configure_generator(self):
+    def send_and_assert(self, key, message, expected_message, max_latency=None):
         """
-        Creates a configuration for a dynamically generated stream of kafka messages
-        :return:
+        Sends a message via the producer, and asserts the expected result from the consumer
+        :param key: key for the kafka message
+        :param message: message content to send
+        :param expected_message: expected message to receive from consumer
+        :param max_latency: fail the test if the message isn't consumed by this time
         """
+        self.__send_message(key, message)
+        self.__get_result(key)
 
-    def send_one(self, key, message):
-        """
-        Send a single, static message via the producer
-        :return:
-        """
+        latency = self.messages[key]['latency']
+        if max_latency is not None and latency > max_latency:
+            assert False, "Latency of {} exceeds max_latency {}".format(latency, max_latency)
 
-        future = self.producer.send(self.producer_topic, key=key, value=message)
-        record_metadata = future.get(timeout=10)
+        consumed_msg = self.messages[key]["output"]
 
+        assert consumed_msg == expected_message, "Consumed message {} does not match expected_message {}" \
+                .format(consumed_msg, expected_message)
+
+    def __send_message(self, key, message):
+        self.producer.send(self.producer_topic, key=key, value=message)
+        self.messages[key] = {'input': message}
+
+    def __get_result(self, key):
         start = time.time()
         timeout_time = start + self.timeout
         while time.time() < timeout_time:
@@ -58,17 +66,15 @@ class KafkaTest:
                 for result in messages:
                     if result.key == key:
                         end = time.time()
-                        result = {'input': message, 'output': result.value, 'latency': end - start,
-                                  'metadata': record_metadata}
-                        self.messages.append(result)
-                        return result
-
+                        self.messages[key]['output'] = result.value
+                        self.messages[key]['latency'] = end - start
+                        return
         raise Exception("Failed to consume message")
 
     def all_messages(self):
         """
-        Returns a formatted string of all messages sent/received
-        :return:
+        Creates a formatted string of all messages sent/received
+        :return: formatted string
         """
         msg_string = ""
         for res in self.messages:
@@ -77,45 +83,6 @@ class KafkaTest:
             msg_string += line
         return msg_string
 
-    def generate_messages(self, count=None):
-        """
-        Generate some messages to the producer
-        :param count: Number of messages to generate, infinite if not specified.
-        :return:
-        """
-
-    def stop(self):
-        """
-        Stop sending messages
-        :return:
-        """
-
-    def assert_next(self, key, message, expected_message, max_latency=None):
-        """
-        Assert that the next message is as expected
-        :param expected:
-        :return:
-        """
-        self.send_one(key, message)
-
-        latency = self.messages[-1]['latency']
-        if max_latency is not None and latency > max_latency:
-            assert False, "Latency of {} exceeds max_latency {}".format(latency, max_latency)
-
-        consumed_msg = self.messages[-1]["output"]
-
-        if consumed_msg == expected_message:
-            assert True
-        else:
-            assert False, "Consumed message {} does not match expected_message {}"\
-                .format(consumed_msg, expected_message)
-
-    def assert_all(self, expected_message, max_latency=None):
-        """
-        Assert all messages
-        :param expected:
-        :return:
-        """
 
 def main():
     kafkatest = KafkaTest()
@@ -125,7 +92,7 @@ def main():
                              auto_offset_reset='earliest',
                              bootstrap_servers=['kafka:9092'])
     kafkatest.configure_consumer(consumer)
-    kafkatest.assert_next(b'1', b'msg', b'msg', max_latency=30)
+    kafkatest.send_and_assert(b'1', b'msg', b'msg', max_latency=30)
 
 if __name__ == "__main__":
     main()
--- tests-unit/test_main.py
+++ tests-unit/test_main.py
@@ -18,22 +18,12 @@ class TestMain:
     @pytest.fixture
     def kafka_test(self, mocker):
         kafka_test = KafkaTest()
-
         self.producer_stub = mocker.stub()
         self.producer_stub.send = mocker.stub()
-        future = mocker.stub()
-        future.get = mocker.stub()
-        future.get.return_value = Message("key",  "test_message_transformed", "topic", 0, 14)
-        self.producer_stub.send.return_value = future
-
         self.consumer_stub = mocker.stub()
         self.consumer_stub.poll = poll
-
-        self.test_msg = Message("key",  "test_message_transformed", "topic", 0, 14)
-
         kafka_test.configure_producer('topic', self.producer_stub)
         kafka_test.configure_consumer(self.consumer_stub)
-
         return kafka_test
 
     def test_configure_producer_sets_topic_and_producer(self, kafka_test):
@@ -43,32 +33,19 @@ class TestMain:
     def test_configure_consumer_sets_consumer(self, kafka_test):
         assert kafka_test.consumer is self.consumer_stub
 
-    def test_send_one_sends_and_receives_message(self, kafka_test, mocker):
-        kafka_test.configure_consumer(self.consumer_stub)
-        kafka_test.send_one("key", "test_message")
-        self.producer_stub.send.assert_called_once()
-        assert kafka_test.messages[0]['input'] is "test_message"
-        assert kafka_test.messages[0]['output'] is "test_message_transformed"
-
-    def test_send_one_sends_and_receive_times_out_exception(self, kafka_test, mocker):
+    def test_assert_next_sends_and_receive_times_out_exception(self, kafka_test, mocker):
         self.consumer_stub.poll = mocker.stub()
         self.consumer_stub.poll.return_value = {"tp": []}
         with pytest.raises(Exception, match="Failed to consume message"):
             kafka_test.configure_consumer(self.consumer_stub)
-            kafka_test.send_one("key", "test_message")
+            kafka_test.send_and_assert("key", "test_message", "test_message_transformed")
             self.producer_stub.send.assert_called_once()
 
-    def test_send_one_reports_latency(self, kafka_test, mocker):
-        kafka_test.configure_consumer(self.consumer_stub)
-        kafka_test.send_one("key", "test_message")
-        self.producer_stub.send.assert_called_once()
-        assert kafka_test.messages[0]['latency'] is not None
-
     def test_assert_next_checks_expected_message_match_when_match(self, kafka_test):
-        kafka_test.assert_next("key", "test_message",  "test_message_transformed", 100)
+        kafka_test.send_and_assert("key", "test_message", "test_message_transformed", 100)
 
     def test_assert_next_checks_expected_message_match_when_match_no_timeout(self, kafka_test):
-        kafka_test.assert_next("key", "test_message",  "test_message_transformed")
+        kafka_test.send_and_assert("key", "test_message", "test_message_transformed")
 
     def test_assert_next_checks_expected_message_match_when_does_not_match_fails(self, kafka_test):
         self.consumer_stub.poll = poll_no_match
@@ -76,14 +53,14 @@ class TestMain:
 
         with pytest.raises(AssertionError, match="Consumed message test_message_transformed_not_" \
                                                  "matching does not match expected_message test_message_transformed"):
-            kafka_test.assert_next("key", "test_message",  "test_message_transformed", 100)
+            kafka_test.send_and_assert("key", "test_message", "test_message_transformed", 100)
 
     def test_assert_next_latency_fails(self, kafka_test):
         self.consumer_stub.poll = poll
         kafka_test.configure_consumer(self.consumer_stub)
 
         with pytest.raises(AssertionError, match="exceeds max_latency 0"):
-            kafka_test.assert_next("key", "test_message",  "test_message_transformed", 0)
+            kafka_test.send_and_assert("key", "test_message", "test_message_transformed", 0)
 
     def test_all_messages_gets_all_sent_and_received(self, kafka_test):
         kafka_test.messages = [{"input": "test_message", "output": "test_message_transformed", "latency": "7.30384"},
